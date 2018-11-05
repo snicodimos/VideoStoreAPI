598c0af9ee0b468c36f029455f71b75996f2e3a0
Goeun Park <goeuntothepark@gmail.com>
2018-11-05 11:53:50 -0800

    add validations in models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :overview, presence: true
+  validates :release_date, presence: true
+  validates :inventory, presence: true 
 end

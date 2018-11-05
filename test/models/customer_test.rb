a80bd95437a05b082fe23238723e8eaf801565df
Semret Nicodimos <snicodimos@gmail.com>
2018-11-05 15:16:38 -0800

    customer validations

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,5 +1,61 @@
 require "test_helper"
 
 describe Customer do
+  describe "validations" do
 
+    it "is valid when all fields are present" do
+      semret = customers(:semret)
+      semret.valid?.must_equal true
+    end
+
+    it "is invalid with a missing title" do
+      semret = customers(:semret)
+      semret.name = nil
+
+      semret.valid?.must_equal false
+    end
+
+    it "is invalid with a missing registered date" do
+      semret = customers(:semret)
+      semret.registered_at = nil
+
+      semret.valid?.must_equal false
+    end
+
+    it "is invalid with a missing address" do
+      semret = customers(:semret)
+      semret.address = nil
+
+      semret.valid?.must_equal false
+    end
+
+    it "is invalid with a missing city" do
+      semret = customers(:semret)
+      semret.city = nil
+
+      semret.valid?.must_equal false
+    end
+
+    it "is invalid with a missing state" do
+      semret = customers(:semret)
+      semret.state = nil
+
+      semret.valid?.must_equal false
+    end
+
+    it "is invalid with a missing postal code" do
+      semret = customers(:semret)
+      semret.postal_code = nil
+
+      semret.valid?.must_equal false
+    end
+
+    it "is invalid with a missing phone" do
+      semret = customers(:semret)
+      semret.phone = nil
+
+      semret.valid?.must_equal false
+    end
+
+  end
 end

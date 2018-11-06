adb36cea9326b604317962439fb4a395dc8d9fb8
Goeun Park <goeuntothepark@gmail.com>
2018-11-06 15:05:23 -0800

    create two tests for checkout in rentals controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,7 +2,23 @@ class RentalsController < ApplicationController
 
   def checkout
     # creating a new instance of a rental
+    rental = Rental.new(rental_params)
+
     # while we check if the avaiable inventory of the movie is not 0
+    movie = Movie.find_by(id: rental_params[:movie_id])
+
+    if movie.available_inventory > 0
+      rental.due_date = Date.today + 7
+      if rental.save
+        render json: {rental_id: rental.id}
+      else
+        render_error(:bad_request, rental.errors.messages)
+      end
+    else
+      render_error(:forbidden, "No movie in available inventory.")
+    end
+
+
     # set the due date (this is a model logic) that will be called here
     # customer checked_out_count will be increamented (model methods)
     # available_inventory will be decremented (model test)
@@ -10,12 +26,17 @@ class RentalsController < ApplicationController
 
   def checkin
     # update rental instance
-      # change status to checkout=false
+    # change status to checkout=false
     # decrement the checked_out_count for customers
     # increament the available_inventory of movies
 
   end
 
+  private
+
+  def rental_params
+    params.permit(:customer_id, :movie_id)
+  end
 
 
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  describe "checkout" do
+    before do
+      @params = {customer_id: customers(:goeun).id, movie_id: movies(:titanic).id}
+    end
+
+    it "creates an instance of Rental with valid data" do
+      expect{
+        post checkout_path(@params)
+      }.must_change "Rental.count", +1
+
+      body = check_response(expected_type: Hash)
+      expect(body).must_include "rental_id"
+    end
+
+    it "returns error if no available movies to rent" do
+      bad_params = {customer_id: customers(:goeun).id, movie_id: movies(:moana).id}
+
+      expect{
+        post checkout_path(bad_params)
+      }.wont_change "Rental.count"
+
+      body = check_response(expected_status: :forbidden, expected_type: Hash)
+
+      expect(body["errors"]).must_include "No movie in available inventory."
+    end
+
+  end
+
+  describe "checkin" do
+  end
 end

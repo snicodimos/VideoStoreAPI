44af08430b8adcd2fdfbe633be8fd17a70db472d
Goeun Park <goeuntothepark@gmail.com>
2018-11-06 07:49:11 -0800

    take out _id in yml, validations testing for rental model green

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,9 +1,34 @@
 require "test_helper"
 
 describe Rental do
-  let(:rental) { Rental.new }
+  describe "validations" do
+    before do
+      @new_rental = Rental.new(customer: customers(:goeun), movie: movies(:titanic))
+    end
 
-  it "must be valid" do
-    value(rental).must_be :valid?
+    it "is valid when all fields are present" do
+      @new_rental.save
+      @new_rental.valid?.must_equal true
+    end
+
+    it "is invalid with a missing customer id" do
+      @new_rental.customer = nil
+      @new_rental.valid?.must_equal false
+    end
+
+    it "is invalid with a missing movie id" do
+      @new_rental.movie = nil
+      @new_rental.valid?.must_equal false
+    end
+
+    it "is invalid with an invalid movie id" do
+      @new_rental.movie_id = 1 #set to likely invalid movie id
+      @new_rental.valid?.must_equal false
+    end
+  end
+
+  describe "relations" do
+
+
   end
 end

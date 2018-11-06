eba61e4f087ece3e6538c34c7cefda74d22d6147
Semret Nicodimos <snicodimos@gmail.com>
2018-11-05 16:39:44 -0800

    added create method for movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,10 +18,21 @@ class MoviesController < ApplicationController
   end
 
   def create
+    movie = Movie.new(movie_params)
+
+    if movie.save
+      render json: {id: movie.id}
+    else
+      render_error(:bad_request, movie.errors.messages)
+    end
 
   end
 
   private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :release_date, :inventory)
+  end
 
   def jsonify_index(movie_data)
     return movie_data.as_json(only: [:id, :title, :release_date])

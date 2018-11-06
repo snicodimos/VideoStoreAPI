2d7b7daca06038e8489f4a314304cee4fc3dd877
Goeun Park <goeuntothepark@gmail.com>
2018-11-06 15:05:02 -0800

    created route shortcuts

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   resources :customers, only: [:index]
   resources :movies, only: [:index, :show, :create]
 
-  post '/rentals/checkin', to: 'rentals#checkin'
+  post '/rentals/checkin', to: 'rentals#checkin', as: 'checkin'
 
-  post '/rentals/checkout', to: 'rentals#checkout'
+  post '/rentals/checkout', to: 'rentals#checkout', as: 'checkout'
+
 
   # get '/zomg', to: 'customers#index'
 

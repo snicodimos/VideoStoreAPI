45928c038a54fb9ba15993ba5b67fc2a5ccdf56c
Semret Nicodimos <snicodimos@gmail.com>
2018-11-07 09:24:39 -0800

    Checkin method in rental controller and checkin test for that method

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -25,11 +25,35 @@ class RentalsController < ApplicationController
   end
 
   def checkin
-    # update rental instance
-    # change status to checkout=false
-    # decrement the checked_out_count for customers
-    # increament the available_inventory of movies
+    # rental = Rental.find_by(rental_params)
 
+    rental = Rental.find_by(customer_id: params[:customer_id], movie_id: params[:movie_id])
+
+    if rental.nil?
+      render_error(:not_found, {id: ["Rental info not found"]})
+    else
+      customer = Customer.find_by(id: rental.customer_id)
+      movie = Movie.find_by(id: rental.movie_id)
+      # update rental instance
+      # change status to checkout=false
+      rental.checkedout = false
+
+      if rental.save
+        # decrement the checked_out_count for customers
+
+        checked_out = customer.movies_checked_out_count
+        customer.update(movies_checked_out_count: checked_out -1)
+
+        # increament the available_inventory of movies
+        inventory = movie.available_inventory
+        movie.update(available_inventory: inventory +1)
+
+        render json: {rental_checkedout: rental.checkedout}, status: :ok
+      else
+        render_error(:bad_request, rental.errors.messages)
+
+      end
+    end
   end
 
   private
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,10 +1,10 @@
 require "test_helper"
 
 describe RentalsController do
+  before do
+    @params = {customer_id: customers(:goeun).id, movie_id: movies(:titanic).id}
+  end
   describe "checkout" do
-    before do
-      @params = {customer_id: customers(:goeun).id, movie_id: movies(:titanic).id}
-    end
 
     it "creates an instance of Rental with valid data" do
       customer = customers(:goeun)
@@ -14,7 +14,7 @@ describe RentalsController do
       pre_movie_count = customer.movies_checked_out_count
 
       expect{
-        post checkout_path(@params)
+        post checkout_path, params: @params
       }.must_change "Rental.count", +1
 
       customer.reload
@@ -32,7 +32,7 @@ describe RentalsController do
       @params[:customer_id] = nil
 
       expect{
-        post checkout_path(@params)
+        post checkout_path, params: @params
       }.wont_change "Rental.count"
 
       body = check_response(expected_status: :bad_request, expected_type: Hash)
@@ -43,7 +43,7 @@ describe RentalsController do
       bad_params = {customer_id: customers(:goeun).id, movie_id: movies(:moana).id}
 
       expect{
-        post checkout_path(bad_params)
+        post checkout_path, params: bad_params
       }.wont_change "Rental.count"
 
       body = check_response(expected_status: :forbidden, expected_type: Hash)
@@ -53,5 +53,29 @@ describe RentalsController do
   end
 
   describe "checkin" do
+    it 'updates rental for valid request' do
+      rental = rentals(:three)
+      customer = rental.customer
+      movie = rental.movie
+
+      pre_inventory = movie.available_inventory
+      pre_movie_count = customer.movies_checked_out_count
+
+
+      post checkin_path, params: {
+        customer_id: rental.customer_id, movie_id: rental.movie_id
+        }
+
+
+      customer.reload
+      movie.reload
+
+      expect(movie.available_inventory).must_equal pre_inventory +1
+
+      expect(customer.movies_checked_out_count).must_equal pre_movie_count -1
+
+      body = check_response(expected_type: Hash)
+      expect(body).must_include "rental_checkedout"
+    end
   end
 end

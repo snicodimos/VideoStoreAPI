5e0e5d79b809c41efd301e828b34c9cb8a30a8cb
Goeun Park <goeuntothepark@gmail.com>
2018-11-07 09:36:57 -0800

    refactor rentals controller test

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,18 +2,14 @@ class RentalsController < ApplicationController
 
   def checkout
     rental = Rental.new(rental_params)
-    customer = Customer.find_by(id: rental_params[:customer_id])
-    movie = Movie.find_by(id: rental_params[:movie_id])
+    customer = find_customer
+    movie = find_movie
 
     if movie.available_inventory > 0
       rental.due_date = Date.today + 7
       if rental.save
-        checked_out = customer.movies_checked_out_count
-        customer.update(movies_checked_out_count: checked_out +1)
 
-        inventory = movie.available_inventory
-        movie.update(available_inventory: inventory - 1)
-
+        rental.rent_movie(customer, movie)
         render json: {rental_id: rental.id}, status: :ok
       else
         render_error(:bad_request, rental.errors.messages)
@@ -21,37 +17,23 @@ class RentalsController < ApplicationController
     else
       render_error(:forbidden, "No movie in available inventory.")
     end
-
   end
 
   def checkin
-    # rental = Rental.find_by(rental_params)
-
     rental = Rental.find_by(customer_id: params[:customer_id], movie_id: params[:movie_id])
 
     if rental.nil?
       render_error(:not_found, {id: ["Rental info not found"]})
     else
-      customer = Customer.find_by(id: rental.customer_id)
-      movie = Movie.find_by(id: rental.movie_id)
-      # update rental instance
-      # change status to checkout=false
+      customer = find_customer
+      movie = find_movie
       rental.checkedout = false
 
       if rental.save
-        # decrement the checked_out_count for customers
-
-        checked_out = customer.movies_checked_out_count
-        customer.update(movies_checked_out_count: checked_out -1)
-
-        # increament the available_inventory of movies
-        inventory = movie.available_inventory
-        movie.update(available_inventory: inventory +1)
-
+        rental.return_movie(customer, movie)
         render json: {rental_checkedout: rental.checkedout}, status: :ok
       else
         render_error(:bad_request, rental.errors.messages)
-
       end
     end
   end
@@ -62,5 +44,11 @@ class RentalsController < ApplicationController
     params.permit(:customer_id, :movie_id)
   end
 
+  def find_customer
+    return Customer.find_by(id: rental_params[:customer_id])
+  end
 
+  def find_movie
+    return Movie.find_by(id: rental_params[:movie_id])
+  end
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -4,4 +4,23 @@ class Rental < ApplicationRecord
 
   validates :customer_id, presence: true
   validates :movie_id, presence: true
+
+
+  def rent_movie(customer, movie)
+    checked_out = customer.movies_checked_out_count
+    customer.update(movies_checked_out_count: checked_out +1)
+
+    inventory = movie.available_inventory
+    movie.update(available_inventory: inventory - 1)
+  end
+
+
+  def return_movie(customer, movie)
+    checked_out = customer.movies_checked_out_count
+    customer.update(movies_checked_out_count: checked_out -1)
+
+    inventory = movie.available_inventory
+    movie.update(available_inventory: inventory +1)
+  end
+
 end
